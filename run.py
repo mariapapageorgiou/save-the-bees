ce69da7130e1f2bde5cc4a123e8d500eb0d40853
Maria Papageorgiou <mariageorgepapas@gmail.com>
2021-08-16 02:06:43 +0000

    Add welcome_message function to run.py

--- run.py
+++ run.py
@@ -1,1 +1,18 @@
-# python code goes here
+def welcome_message():
+    """
+    Welcome function to print a message to the user
+    """
+    print()
+    print("##############################################")
+    print("#                                            #")
+    print("#                    SAVE                    #")
+    print("#                    THE                     #")
+    print("#                    BEES                    #")
+    print("#                                            #")
+    print("#         Creator Maria Papageorgiou         #")
+    print("#                                            #")
+    print("##############################################\n\n")
+    print("Add welcome message here\n")
+
+
+welcome_message()
